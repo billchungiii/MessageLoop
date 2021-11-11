d3c5818ecda8502cb7b318a0e6da80435f96efc8
Bill Chung <bill_chungiii@hotmail.com>
2021-11-11 21:26:48 +0800

    加入專案檔案。

---
+++ MessageLoopLibrary/SingleThreadWorker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageLoopLibrary
+{
+    public sealed class SingleThreadWorker
+    {
+        private Thread _thread;
+        private AutoResetEvent _resetEvent;
+        public bool IsRunning { get; private set; }
+
+        private ConcurrentQueue<(object target, Delegate method, object[] args, Action<object> callback)> _delegates;
+
+        public SingleThreadWorker()
+        {
+            IsRunning = true;
+            _delegates = new ConcurrentQueue<(object target, Delegate method, object[] args, Action<object> callback)>();
+            _resetEvent = new AutoResetEvent(false);
+            _thread = new Thread(RunMessageLoop);
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
+
+        public void Call(object target, Delegate method, object[] args, Action<object> callback)
+        {
+            _delegates.Enqueue((target, method, args, callback));
+            _resetEvent.Set();
+        }
+
+        private void Stop()
+        {
+            IsRunning = false;
+            _resetEvent?.Set();
+        }
+
+        private void RunMessageLoop()
+        {
+            while (IsRunning)
+            {
+                while (_delegates.TryDequeue(out (object target, Delegate method, object[] args, Action<object> callback) item))
+                {
+                    object result = item.method.Method.Invoke(item.target, item.args);
+                    item.callback?.Invoke(result);
+                }
+
+                if (_delegates.Count == 0 && IsRunning)
+                {
+                    _resetEvent.WaitOne();
+                }
+            }
+        }
+
+        ~SingleThreadWorker()
+        {
+            Stop();
+        }
+
+    }
+}
---
+++ MessageLoopSample001/Program.cs
@@ -0,0 +1,44 @@
+﻿using MessageLoopLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageLoopSample001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new SingleThreadWorker();
+            worker.Call(null,new Action<string> (Method001), new object[] { nameof(worker) }, null);
+            worker.Call(null, new Func<string,int>(Method002), new object[] { nameof(worker) }, (x) =>  Console.WriteLine($" count is  {x}"));
+            worker.Call(null, new Func<string,int>(Method002), new object[] { nameof(worker) }, (x) => Console.WriteLine($" count is  {x}"));
+
+           
+
+            var anotherWroker = new SingleThreadWorker();
+            anotherWroker.Call(null, new Action<string>(Method001), new object[] { nameof(anotherWroker) }, null);
+            anotherWroker.Call(null, new Func<string, int>(Method002), new object[] { nameof(anotherWroker) }, (x) => Console.WriteLine($" count is  {x}"));
+            anotherWroker.Call(null, new Func<string, int>(Method002), new object[] { nameof(anotherWroker) }, (x) => Console.WriteLine($" count is  {x}"));
+            Console.ReadLine();
+        }
+
+        static int _count = 0;
+        static void Method001(string caller)
+        {
+            Console.WriteLine($"Method 001 running in Thread Id {Thread.CurrentThread.ManagedThreadId} by {caller}");
+        }
+
+        static int Method002(string caller)
+        {
+            _count++;
+            Console.WriteLine($"Method 002 running in Thread Id {Thread.CurrentThread.ManagedThreadId}  by {caller}");
+            return _count;
+        }
+
+    }
+}
